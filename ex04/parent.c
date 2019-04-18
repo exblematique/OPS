f247ee4402549a5a32b34922fbd3db817d3e3d54
Yoann <ychappaz.ir2021@esaip.org>
2019-04-18 12:14:30 +0200

    4a done

---
+++ ex04/parent.c
@@ -0,0 +1,52 @@
+/******************************************************************************
+ * File:         display.c
+ * Version:      1.4
+ * Date:         2018-02-20
+ * Author:       M. van der Sluys, J.H.L. Onokiewicz, R.B.A. Elsinghorst, J.G. Rouland
+ * Description:  OPS exercise 2: syntax check
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <unistd.h>
+#include "displayFunctions.h"
+
+int main(int argc, char *argv[]) {
+  unsigned long int numOfTimes, niceIncr;
+  char printMethod, printChar;
+  ErrCode err;
+  
+  err = SyntaxCheck(argc, argv);  // Check the command-line parameters
+  if(err != NO_ERR) {
+    DisplayError(err);        // Print an error message
+  } else {
+      printMethod = argv[1][0];
+      numOfTimes = strtoul(argv[2], NULL, 10);  // String to unsigned long
+      niceIncr = strtoul(argv[3], NULL, 10);
+
+      int idChild;
+      int nbChild = argc-4;
+      for (int iChild=0; iChild<nbChild; iChild++){
+	idChild = fork();
+	if (idChild != 0) {
+	  continue;
+	}
+	printChar = argv[iChild+4][0];
+	nice(iChild*niceIncr);
+	printf("iChild= %d, iChild*niceIncr= %ld, iChild-th= %c, priority= %d\n", iChild, iChild*niceIncr, printChar, getpriority(PRIO_PROCESS, 0));
+	PrintCharacters(printMethod, numOfTimes, printChar);  // Print character
+
+	break;
+      }
+      if (idChild != 0){
+	for (int i=0; i<nbChild; i++){ 
+	  printf("Child with PID %d has stopped\n", wait(NULL));
+	}
+      }
+  }
+  printf("\n");  // Newline at end
+  return 0;
+}
