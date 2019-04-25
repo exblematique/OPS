d568ee6fd00360f57588864eb2ed5eab7c8412cf
Yoann <ychappaz.ir2021@esaip.org>
2019-04-25 11:48:22 +0200

    4b done

--- ex04/parent.c
+++ ex04/parent.c
@@ -15,16 +15,15 @@
 #include "displayFunctions.h"
 
 int main(int argc, char *argv[]) {
-  unsigned long int numOfTimes, niceIncr;
-  char printMethod, printChar;
+  unsigned long int niceIncr;
+  char *printChar;
   ErrCode err;
   
   err = SyntaxCheck(argc, argv);  // Check the command-line parameters
   if(err != NO_ERR) {
     DisplayError(err);        // Print an error message
   } else {
-      printMethod = argv[1][0];
-      numOfTimes = strtoul(argv[2], NULL, 10);  // String to unsigned long
+
       niceIncr = strtoul(argv[3], NULL, 10);
 
       int idChild;
@@ -34,17 +33,18 @@ int main(int argc, char *argv[]) {
 	if (idChild != 0) {
 	  continue;
 	}
-	printChar = argv[iChild+4][0];
+	printChar = argv[iChild+4];
 	nice(iChild*niceIncr);
-	printf("iChild= %d, iChild*niceIncr= %ld, iChild-th= %c, priority= %d\n", iChild, iChild*niceIncr, printChar, getpriority(PRIO_PROCESS, 0));
-	PrintCharacters(printMethod, numOfTimes, printChar);  // Print character
-
+	printf("iChild= %d, iChild*niceIncr= %ld, iChild-th= %c, priority= %d\n", iChild, iChild*niceIncr, printChar[0], getpriority(PRIO_PROCESS, 0));
+	execl("../ex02/display", "./display", argv[1], argv[2], printChar, (char*) NULL);  // Print character
+	perror("Perror: ");
 	break;
       }
       if (idChild != 0){
 	for (int i=0; i<nbChild; i++){ 
 	  printf("Child with PID %d has stopped\n", wait(NULL));
 	}
+	printf("All children have finished");
       }
   }
   printf("\n");  // Newline at end
