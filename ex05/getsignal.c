8275740e9b4d9308a2cfe541787c4992b6c9b9b6
Yoann <ychappaz.ir2021@esaip.org>
2019-05-09 11:38:04 +0200

    5b done

---
+++ ex05/getsignal.c
@@ -0,0 +1,42 @@
+/******************************************************************************
+ * File:         getsignal.c
+ * Version:      0.1
+ * Date:         2019-04-25
+ * Author:       Yoann Chappaz
+ * Description:  OPS exercise 5: signals for Synchronisation
+ ******************************************************************************/
+
+#define _POSIX_C_SOURCE 199309L
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+
+char digit = '0';
+void changeDigit(int sig);
+
+int main(int argc, char *argv[]) {
+  
+  struct sigaction signal;
+
+  //Define signal
+  memset(&signal, '\0', sizeof(signal));
+  signal.sa_handler = changeDigit;
+  signal.sa_flags = 0;
+  sigemptyset(&signal.sa_mask);
+  sigaction(25, &signal, NULL);
+  
+  while (1){
+    write(1, &digit, sizeof(digit));
+    sleep(1);
+  }
+  return 0;
+}
+
+void changeDigit(int sig){
+  if (digit == '9') {
+    digit = '0';
+  } else {
+    digit += 1;
+  }
+}  
